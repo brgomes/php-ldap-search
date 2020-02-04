094d60cbf7aeb8c03846b6e7d3a1601bfe871fc2
Bruno Roberto Gomes <bruno@brgomes.com>
2020-02-04 16:14:57 -0300

    Initial commit

--- index.php
+++ index.php
@@ -20,29 +20,29 @@ define('PASSWORD', '');
 $conn = ldap_connect(LDAP_HOST, LDAP_PORT);
 
 if ($conn) {
-    ldap_set_option($conn, LDAP_OPT_PROTOCOL_VERSION, 3);
-    ldap_set_option($conn, LDAP_OPT_REFERRALS, 0);
-    ldap_set_option($conn, LDAP_OPT_DEBUG_LEVEL, 7);
+	ldap_set_option($conn, LDAP_OPT_PROTOCOL_VERSION, 3);
+	ldap_set_option($conn, LDAP_OPT_REFERRALS, 0);
+	ldap_set_option($conn, LDAP_OPT_DEBUG_LEVEL, 7);
 
-    if (@ldap_bind($conn, USERNAME . LDAP_DOMAIN, PASSWORD)) {
-	    $filter = 'samaccountname=' . USERNAME;
-	    $fields = array('samaccountname', 'memberof', 'displayname');
+	if (@ldap_bind($conn, USERNAME . LDAP_DOMAIN, PASSWORD)) {
+		$filter = 'samaccountname=' . USERNAME;
+		$fields = array('samaccountname', 'memberof', 'displayname');
 
-	    $user  	= ldap_search($conn, LDAP_DN, $filter, $fields);
-	    $count 	= ldap_count_entries($conn, $user);
+		$user  	= ldap_search($conn, LDAP_DN, $filter, $fields);
+		$count 	= ldap_count_entries($conn, $user);
 
-	    if ($count == 1) {
-	    	$entries = ldap_get_entries($conn, $user);
+		if ($count == 1) {
+			$entries = ldap_get_entries($conn, $user);
 
-	    	echo '<div style="line-height:30px; border:1px solid green; padding:10px 15px; background-color:#C5FFB8">';
-	    	echo 'sAMAccountName: <strong>', $entries[0]['samaccountname'][0] , '</strong><br />';
-	    	echo 'Display name: <strong>', $entries[0]['displayname'][0] , '</strong><br />';
-	    	echo 'Member of: <strong>', $entries[0]['memberof'][0] , '</strong><br />';
-	    	echo '</div>';
-	    }
+			echo '<div style="line-height:30px; border:1px solid green; padding:10px 15px; background-color:#C5FFB8">';
+			echo 'sAMAccountName: <strong>', $entries[0]['samaccountname'][0] , '</strong><br />';
+			echo 'Display name: <strong>', $entries[0]['displayname'][0] , '</strong><br />';
+			echo 'Member of: <strong>', $entries[0]['memberof'][0] , '</strong><br />';
+			echo '</div>';
+		}
 	} else {
 		echo '<div style="line-height:30px; border:1px solid red; padding:10px 15px; background-color:#FFBBBB">';
-    	echo 'Error: '. ldap_error($conn);
-    	echo '</div>';
+		echo 'Error: '. ldap_error($conn);
+		echo '</div>';
 	}
 }
